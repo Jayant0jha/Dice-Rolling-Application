e195342b8ea5df731481f4a8d1b937abf29b508f
Jayant0jha <31176672+Jayant0jha@users.noreply.github.com>
2018-07-17 18:15:01 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+
+
+function dice(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = dice(1, 6);
